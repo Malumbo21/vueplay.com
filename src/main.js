01fcc6cb9810b9d7fd27c6496e9d6d27a664dfe6
Vue Play <steffanhalv@gmail.com>
2023-07-17 13:42:07 +0200

    Stored by Vue Play 1689594127396

--- src/main.js
+++ src/main.js
@@ -4,9 +4,38 @@ import {
 } from 'vue'
 import App from '@/App.vue'
 import router from '@/logic/router'
+import feathers from '@feathersjs/client'
+import sio from 'socket.io-client'
 
 const app = createApp(App)
 
-app.use(router)
+const socket = sio('https://api.vueplay.io', {
+    transports: ['websocket', 'polling']
+})
 
-app.mount('#app')
+const io = feathers()
+io.configure(feathers.socketio(socket))
+io.configure(feathers.authentication())
+
+let boot = async () => {
+
+    try {
+        await io.reAuthenticate()
+    } catch (e) { }
+
+    router.beforeEach(async () => {
+        try {
+            let user = await io.reAuthenticate()
+            localStorage.setItem('user', JSON.stringify(user))
+        } catch (e) {
+            localStorage.setItem('user', null)
+        }
+    })
+
+    app.provide('io', io)
+    app.use(router)
+    app.mount('#app')
+
+}
+
+boot()
