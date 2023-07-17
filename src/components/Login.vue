a2a47ad662746f69a392c2cc32fda8e703102515
Vue Play <steffanhalv@gmail.com>
2023-07-17 13:31:19 +0200

    Stored by Vue Play 1689593479894

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+<script>
+    export default {
+        expose: ['login'],
+        data: () => ({
+
+        }),
+        methods: {
+            login() {
+                alert('Make login here!')
+            }
+        }
+    };
+</script>
+<style scoped></style>
