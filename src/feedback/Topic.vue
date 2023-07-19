78f74096e43a308080ca507c83234f4a196d7473
Vue Play <steffanhalv@gmail.com>
2023-07-19 14:06:34 +0200

    Stored by Vue Play 1689768394984

---
+++ src/feedback/Topic.vue
@@ -0,0 +1,131 @@
+<template>
+    <section class="p-6 md:pt-6 pb-24">
+        <Meta title="Frequently Asked Questions" description="Vue Play was created to simplify web development in a visual way, inspired by several tools like Elementor, Adobe Photoshop, Adobe Flash and GameMaker.">
+        </Meta>
+        <Login ref="auth" @user="authenticated" @cancel="cancelled">
+        </Login>
+        <div class="flex-col max-w-2xl mx-auto lg:mb-8 flex text-lg lg:flex-row">
+            <div class="font-light">
+                <span>
+                    Showing
+                </span><select name="" class="cursor-pointer bg-transparent underline-offset-4 underline mx-1">
+                    <option>
+                        Latest
+                    </option>
+                </select><span>
+                    posts in
+                </span><select name="" class="cursor-pointer bg-transparent underline-offset-4 underline ml-1">
+                    <option>
+                        All Categories
+                    </option>
+                </select>
+            </div>
+            <div class="lg:grow">
+                <form class="lg:float-right mt-4 lg:mt-0">
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div> <input type="search" id="default-search" class="w-full h-10 lg:w-52 lg:h-full block max-w-full pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required="" />
+                    </div>
+                </form>
+            </div>
+        </div>
+        <article v-for="post in posts" class="max-w-2xl mx-auto mt-4 flex">
+            <div class="w-16 inline-flex">
+                <div class="border w-11 h-12 rounded-lg">
+                    <div class="h-1/2 w-full">
+                        <svg viewBox="0 0 20 10" class="pt-2 w-5 h-5 fill-gray-400 mx-auto">
+                            <polygon points="10,0 20,10 0,10" />
+                        </svg>
+                    </div>
+                    <div class="text-center h-1/2 w-full -mt-1">
+                        {{ post.votes?.length }}
+                    </div>
+                </div>
+            </div>
+            <div class="overflow-hidden text-ellipsis max-h-20 flex-col inline-flex grow mr-4">
+                <h1 class="pt-2.5 text-xl font-semibold">
+                    {{ post.title }} {{ post.votes?.length }}
+                </h1>
+            </div>
+        </article>
+        <article v-for="post in posts" class="max-w-2xl mx-auto mt-4 flex">
+            <div class="w-16 inline-flex">
+                <div class="w-11">
+                    <div class="h-7 w-7 bg-cover rounded-full bg-slate-500 mx-auto">
+                    </div>
+                </div>
+            </div>
+            <div class="overflow-hidden text-ellipsis max-h-20 flex-col inline-flex grow mr-4">
+                <h1 class="font-semibold mt-0.5">
+                    {{ post.user[0].email }}
+                </h1>
+            </div>
+        </article>
+        <article v-for="post in posts" class="max-w-2xl mx-auto mb-8 mt-4 flex">
+            <div class="w-16 inline-flex">
+                <div class="w-11"> </div>
+            </div>
+            <div class="overflow-hidden text-ellipsis max-h-20 flex-col inline-flex grow mr-4">
+                <p class="mb-3">
+                    {{ post.description }}
+                </p>
+                <p class="text-gray-500 text-sm">
+                    {{ post.createdAt }}
+                </p>
+            </div>
+        </article>
+    </section>
+</template>
+<script>
+    import Meta from "@/components/Meta.vue";
+    import Login from "@/components/Login.vue";
+    export default {
+        components: {
+            Meta,
+            Login
+        },
+        inject: ["io", "user"],
+        data: () => ({
+            posts: []
+        }),
+        created() {
+            this.getPosts()
+        },
+        methods: {
+            authenticated(user) {
+                alert("Authenticated!");
+                console.log("authenticated", user);
+                console.log("global user", this.user)
+            },
+            cancelled() {
+                alert("Cancelled authentication!");
+                console.log("global user", this.user)
+            },
+            async getPosts() {
+                this.posts = await this.io.service("types/feedback").find({ // query: {}
+                })
+            },
+            async vote(id) {
+                if (this.user.value) {
+                    await this.io.service("types/feedback").patch(id, {
+                        vote: true
+                    });
+                    await this.getPosts();
+                    alert("Voted!")
+                } else {
+                    await this.$refs.auth.login();
+                    console.log("Logged in?", this.user)
+                }
+            },
+            async remove(id) {
+                await this.io.service("types/feedback").remove(id);
+                await this.getPosts();
+                alert("Removed!")
+            }
+        }
+    };
+</script>
+<style scoped></style>
