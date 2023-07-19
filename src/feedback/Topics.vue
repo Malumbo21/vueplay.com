add6a64e1a440d17087ff0f6ba2bc8f3fef8cdcc
Vue Play <steffanhalv@gmail.com>
2023-07-19 09:58:10 +0200

    Stored by Vue Play 1689753490435

--- src/feedback/Topics.vue
+++ src/feedback/Topics.vue
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </div>
-            <div class="flex-col inline-flex grow mr-4">
+            <div class="overflow-hidden text-ellipsis max-h-20 flex-col inline-flex grow mr-4">
                 <h1 class="font-semibold">
                     {{ post.title }} {{ post.votes?.length }}
                 </h1>
