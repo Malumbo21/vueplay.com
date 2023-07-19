04ecfcb8367ae82a64725debeac70f259d9048bb
Vue Play <steffanhalv@gmail.com>
2023-07-19 17:01:53 +0200

    Stored by Vue Play 1689778913457

--- src/feedback/Topics.vue
+++ src/feedback/Topics.vue
@@ -21,8 +21,8 @@
                 </select>
             </div>
             <div class="lg:grow">
-                <form class="lg:float-right mt-4 lg:mt-0">
-                    <div class="relative">
+                <form class="lg:float-right mt-4 lg:mt-0 h-full">
+                    <div class="relative h-full">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
