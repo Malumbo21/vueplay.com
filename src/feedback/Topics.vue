6bce26daad4a8ccffb956ed78cae107becddeb64
Vue Play <steffanhalv@gmail.com>
2023-07-19 09:14:04 +0200

    Stored by Vue Play 1689750844055

--- src/feedback/Topics.vue
+++ src/feedback/Topics.vue
@@ -13,7 +13,7 @@
                         </svg>
                     </div>
                     <div class="text-center h-1/2 w-full -mt-1">
-                        45
+                        {{ post.votes?.length }}
                     </div>
                 </div>
             </div>
@@ -22,16 +22,15 @@
                     {{ post.title }} {{ post.votes?.length }}
                 </h1>
                 <p class="text-gray-500">
-                    {{ post.description }} {{ post.comments?.length }}
+                    {{ post.description }}
                 </p><button @click="remove(post._id)">Delete</button>
             </div>
             <div class="w-12 inline-flex">
                 <div class="w-1/2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="stroke-gray-400 w-5 h-5 mt-1">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                    </svg>
-                </div>
+                    </svg> </div>
                 <div class="w-1/2 text-right">
-                    45
+                    {{ post.comments?.length }}
                 </div>
             </div>
         </article>
