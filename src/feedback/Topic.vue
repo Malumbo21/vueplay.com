3675d7e244fa62b913e880f0f9d3593b13346433
Vue Play <steffanhalv@gmail.com>
2023-07-19 14:50:12 +0200

    Stored by Vue Play 1689771012745

--- src/feedback/Topic.vue
+++ src/feedback/Topic.vue
@@ -44,9 +44,20 @@
                 <div class="grow">
                     <p class="text-gray-700 mb-3">
                         {{ post.description }}
-                    </p> <span class="text-gray-500 text-xs">
-                        {{ moment(post.createdAt).format('MMMM DD, YYYY') }}
-                    </span>
+                    </p>
+                    <div class="flex text-gray-500">
+                        <span class="text-xs">
+                            {{ moment(post.createdAt).format('MMMM DD, YYYY') }}
+                        </span> <span class="text-xs ml-1">
+                            ·
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer inline-flex -mt-1">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                            </svg> </span> <span class="text-xs ml-1">
+                            ·
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer inline-flex -mt-0.5">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                            </svg> </span>
+                    </div>
                     <div class="mt-8">
                         <form class="lg:float-right mt-4 lg:mt-0">
                             <div class="relative">
@@ -62,7 +73,7 @@
                 </div>
             </div>
         </article>
-        <article>
+        <article v-for="i in 5">
             <div v-for="post in posts" class="max-w-2xl mx-auto mt-4 flex">
                 <div class="w-16 inline-flex">
                     <div class="w-11">
@@ -103,38 +114,6 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                             </svg>
 
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </article>
-        <article>
-            <div v-for="post in posts" class="max-w-2xl mx-auto mt-4 flex">
-                <div class="w-16 inline-flex">
-                    <div class="w-11">
-                        <div class="h-7 w-7 bg-cover rounded-full bg-slate-500 mx-auto">
-                        </div>
-                    </div>
-                </div>
-                <div class="overflow-hidden text-ellipsis max-h-8 flex-col inline-flex grow">
-                    <h1 class="font-semibold mt-0.5">
-                        {{ post.user[0].email }}
-                    </h1>
-                </div>
-            </div>
-            <div v-for="post in posts" class="mx-auto mb-8 mt-2 flex">
-                <div class="w-16 inline-flex">
-                    <div class="w-11"> </div>
-                </div>
-                <div class="grow">
-                    <p class="text-gray-700 mb-3">
-                        {{ post.description }}
-                    </p>
-                    <div class="flex text-gray-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1 cursor-pointer">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                        </svg><span class="text-xs">
-                            · {{ moment(post.createdAt).format('MMMM DD, YYYY') }}
                         </span>
                     </div>
                 </div>
