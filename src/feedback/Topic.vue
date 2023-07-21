aee67ca4efcd974562f37a859b2ebf22ee13ca11
Vue Play <steffanhalv@gmail.com>
2023-07-21 14:58:46 +0200

    Stored by Vue Play 1689944326307

--- src/feedback/Topic.vue
+++ src/feedback/Topic.vue
@@ -77,7 +77,7 @@
                 </div>
             </div>
         </article>
-        <article v-for="comment in post?.comments?.data">
+        <article v-for="comment in post?.comments">
             <div class="max-w-2xl mx-auto mt-4 flex">
                 <div class="w-16 inline-flex">
                     <div class="w-11">
@@ -98,10 +98,12 @@
                 <div class="grow">
                     <p class="text-gray-700 mb-3">
                         {{ comment.comment }}
-                    </p><textarea v-model="comment.description" rows="" cols="" class="w-full h-16 mb-3 border">
+                    </p><textarea v-model="comment.comment" rows="" cols="" class="w-full h-16 mb-3 border">
 </textarea>
                     <div class="flex text-gray-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1 cursor-pointer" @click="like(comment)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 mr-1 cursor-pointer" @click="like(comment)" :class="{
+'fill-red-400': comment.likes?.length,'stroke-red-300': comment.likes?.length
+}">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                         </svg> <span class="text-xs">
                             {{comment.likes?.length &gt; 1 ? comment.likes?.length + ' likes' : ''}}{{comment.likes?.length === 1 ? comment.likes?.length + ' like' : ''}} · {{ moment(comment.createdAt).format('MMMM DD, YYYY') }}
@@ -175,7 +177,8 @@
                 }
             },
             async refresh() {
-                this.post = await this.io.service("types/feedback").get(this.id)
+                this.post = await this.io.service("types/feedback").get(this.id);
+                console.log("post", this.post)
             },
             async myVote(post) {
                 const votes = await this.io.service("types/feedback-votes").find({
