de5f1009c0df3c2f79f666e4c39915ce8daa541d
Vue Play <steffanhalv@gmail.com>
2023-07-21 18:16:12 +0200

    Stored by Vue Play 1689956172044

--- src/feedback/Topic.vue
+++ src/feedback/Topic.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="p-6 md:pt-6 pb-24">
         <Meta :title="post?.title" :description="post?.description" />
-        <div class="h-16 p-2 bg-slate-50 rounded">
+        <div class="h-16 mb- p-2 bg-slate-50 rounded">
             <div class="h-12 w-12 bg-cover rounded-full bg-slate-500 mx-auto float-right" :style="'background-image: url(' + (user?.picture || user?.value?.picture) + ')'" v-if="user?.email || user?.value?.email">
             </div>
             <div class="float-right mt-3 mr-4" v-if="user?.email || user?.value?.email">
--- src/feedback/Topics.vue
+++ src/feedback/Topics.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="p-6 md:pt-6 pb-24">
-        <div class="h-16 p-2 bg-slate-50 rounded">
+        <div class="h-16 mb-4 p-2 bg-slate-50 rounded">
             <div class="h-12 w-12 bg-cover rounded-full bg-slate-500 mx-auto float-right" :style="'background-image: url(' + (user?.picture || user?.value?.picture) + ')'" v-if="user?.email || user?.value?.email">
             </div>
             <div class="float-right mt-3 mr-4" v-if="user?.email || user?.value?.email">
