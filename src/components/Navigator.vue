f7032f298c67f248cfc0fe55f304d17a362d25f7
Vue Play <steffanhalv@gmail.com>
2023-07-21 21:37:31 +0200

    Stored by Vue Play 1689968251484

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,25 +1,43 @@
 <template>
     <nav style="background-image:linear-gradient(204deg, #f0dfe20d 54%, #e1ebe8ff 59%, #f0f5faff 74%);background-color:#edf7f4ff;min-height:50px" class="shadow border-slate-300 border-b">
         <div style="width:216px;background-size:contain;background-repeat:no-repeat;margin-left:10px;cursor:pointer;float:left;height:35px;margin-top:7px;margin-bottom:7px" @click="$router.push('/')" class="sm:inline hidden logo">
-        </div><button @click="$router.push('/')" :class="{active: route === '/'}" class="sm:hidden">
+        </div><button @click="$router.push('/')" :class="{active: route === '/'}" class="hover:text-green-700 text-slate-700 pr-2 sm:hidden">
             Home
-        </button><button @click="$router.push('/features')" :class="{active: route === '/features'}" class="hover:text-green-700 text-slate-700">
+        </button><button @click="$router.push('/features')" :class="{active: route === '/features'}" class="hover:text-green-700 text-slate-700 px-2">
             Features
-        </button><button @click="$router.push('/documentation')" :class="{active: route.includes('/documentation')}" class="hover:text-green-700 text-slate-700">
-            Docs
-        </button><button @click="$router.push('/pricing')" :class="{active: route === '/pricing'}" class="hover:text-green-700 text-slate-700">
+        </button><button @click="$router.push('/pricing')" :class="{active: route === '/pricing'}" class="hover:text-green-700 text-slate-700 px-2">
             Pricing
-        </button> <button @click="open('https://studio.vueplay.com')" :class="{active: route === '/app'}" style="font-weight:bold;border-top-left-radius:4px;border-bottom-left-radius:4px;height:29px;border-top-right-radius:4px;border-bottom-right-radius:4px;margin-right:5px" class="hover:bg-violet-600 bg-violet-500 from-blue-500 bg-gradient-to-br hover:from-blue-600 text-white duration-300 transition hover:drop-shadow-lg drop-shadow">
+        </button> <button @click="submenu=true" :class="{active: subRoute}" class="relative hover:text-green-700 text-slate-700 pr-2" @mouseenter="submenu=true" @mouseleave="submenu=false">
+            <span>
+                More...
+            </span>
+            <div class="border-y-emerald-100 border-t border-b-2 right-0 top-full absolute bg-emerald-50 shadow pb-2" v-if="submenu">
+                <button @click="submenu=false, $router.push('/feedback')" :class="{active: route.includes('/feedback')}" class="px-4 py-2 w-full hover:text-green-700 text-slate-700 text-right">
+                    Feedback
+                </button> <button @click="submenu=false, $router.push('/documentation')" :class="{active: route.includes('/documentation')}" class="px-4 py-2 w-full hover:text-green-700 text-slate-700 text-right">
+                    Documentation
+                </button>
+            </div>
+        </button> <button @click="open('https://studio.vueplay.com')" :class="{active: route === '/app'}" style="font-weight:bold;border-top-left-radius:4px;border-bottom-left-radius:4px;height:29px;border-top-right-radius:4px;border-bottom-right-radius:4px;margin-right:5px" class="px-3 hover:bg-violet-600 bg-violet-500 from-blue-500 bg-gradient-to-br hover:from-blue-600 text-white duration-300 transition hover:drop-shadow-lg drop-shadow">
             Studio ⇾
         </button>
     </nav>
 </template>
 <script>
     export default {
-        data: () => ({}),
+        data: () => ({
+            submenu: false
+        }),
         computed: {
             route() {
                 return this.$route.path
+            },
+            subRoute() {
+                const routes = ["/documentation", "/feedback"];
+                for (const route of routes) {
+                    if (this.$route.path.includes(route)) return true
+                }
+                return false
             }
         },
         methods: {
@@ -42,8 +60,6 @@
 
     button {
         border-style: none;
-        padding-right: 10px;
-        padding-left: 10px;
         cursor: pointer;
         transition: .3s ease color;
         height: 100%;
@@ -51,6 +67,10 @@
 
     button.active {
         font-weight: bold;
+    }
+
+    button.active div {
+        font-weight: normal;
     }
 
     .logo {
