6bf8261e9e3afb98b62bec2d106da1e10f7e41c1
Vue Play <steffanhalv@gmail.com>
2023-07-31 10:24:35 +0200

    Stored by Vue Play 1690791875369

--- src/search/Component.vue
+++ src/search/Component.vue
@@ -60,10 +60,12 @@
                     <p class="text-gray-700 mb-3" v-if="!edit">
                         {{ post?.description }}
                     </p> <textarea v-model="post.description" rows="" cols="" class="rounded w-full h-32 mb-3 border" v-else="">
-</textarea>
+</textarea><button class="mt-2 cursor-pointer mr-2 bg-slate-50 hover:bg-slate-100 shadow rounded px-2 py-2 inline-block" title="Toggle Tailwind" @click="tailwind=!tailwind">
+                        Toggle Tailwind
+                    </button>
                     <h2 class="font-medium my-3">
-                        Component Preview (Including Tailwind)
-                    </h2><iframe title="" class="shadow bg-slate-50 aspect-square w-full mb-4" :src="'https://api.vueplay.io/types/sfc/' + post._id + '?ref=HEAD&type=preview-tw'">
+                        Component Preview ({{ tailwind ? 'Including' : 'Excluding'}} Tailwind)
+                    </h2><iframe title="" class="shadow bg-slate-50 aspect-square w-full mb-4" :src="'https://api.vueplay.io/types/sfc/' + post._id + '?ref=HEAD&type=preview' + (tailwind ? '-tw' : '')">
                     </iframe>
                     <div class="mb-4 w-full block">
                         <h2 class="font-medium my-3">
@@ -98,7 +100,7 @@
                     </div>
                     <div class="mb-4 w-full block">
                         <h2 class="font-medium my-3">
-                            Get source code including tailwind
+                            Get source code including tailwind by CDN
                         </h2><a class="cursor-pointer mr-2 bg-slate-50 hover:bg-slate-100 shadow rounded px-2 mb-2 py-2 inline-block" target="_blank" :href="'https://api.vueplay.io/types/sfc/' + post._id + '?ref=HEAD&type=preview-tw'" title="Preview Component Including Tailwind">
                             Preview Tailwind
                         </a><a class="cursor-pointer mr-2 bg-slate-50 hover:bg-slate-100 shadow rounded px-2 mb-2 py-2 inline-block" target="_blank" :href="'https://api.vueplay.io/types/sfc/' + post._id + '?ref=HEAD&type=preview-wc-tw'" title="Preview Web Component Including Tailwind">
@@ -207,6 +209,7 @@
             }
         },
         data: () => ({
+            tailwind: true,
             comment: "",
             edit: false,
             post: null,
