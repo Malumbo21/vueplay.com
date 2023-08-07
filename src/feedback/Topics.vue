b9af9f002cc50321c5cdf118dea63dd231c89d93
Vue Play <steffanhalv@gmail.com>
2023-08-07 17:08:22 +0200

    Stored by Vue Play 1691420902184

--- src/feedback/Topics.vue
+++ src/feedback/Topics.vue
@@ -51,7 +51,7 @@
                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
-                        </div> <input type="search" id="default-search" class="lg:w-48 w-full h-10 lg:h-full max-w-full pl-10 pr-2 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required="" v-model="term" @change="refresh()" />
+                        </div> <input type="search" id="default-search" class="lg:w-44 w-full h-10 lg:h-full max-w-full pl-10 pr-2 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..." required="" v-model="term" @change="refresh()" />
                     </div>
                 </div>
             </div>
