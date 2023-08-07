1f92598e3482107bfb7f7193a98a36bdfdddddd9
Vue Play <steffanhalv@gmail.com>
2023-08-07 16:16:09 +0200

    Stored by Vue Play 1691417769334

--- src/feedback/Topic.vue
+++ src/feedback/Topic.vue
@@ -55,7 +55,11 @@
                     <p class="text-gray-700 mb-3" v-if="!edit">
                         {{ post?.description }}
                     </p> <textarea v-model="post.description" rows="" cols="" class="rounded w-full h-32 mb-3 border" v-else="">
-</textarea><img class="rounded w-full mb-4" :src="post.screenshot" v-if="post?.screenshot" /> <button class="mr-2 bg-slate-50 hover:bg-slate-100 shadow rounded px-2 mb-2 py-2" @click="selectImage" v-if="edit">
+</textarea><select v-model="post.status_id" class="w-full h-10 block max-w-full pl-4 pr-2 text-sm text-gray-900 placeholder:text-slate-500 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" v-if="edit && user?.permissions?.includes('admin')">
+                        <option v-for="status in statuses" :value="status._id">
+                            {{status.title}}
+                        </option>
+                    </select><img class="rounded w-full mb-4" :src="post.screenshot" v-if="post?.screenshot" /> <button class="mr-2 bg-slate-50 hover:bg-slate-100 shadow rounded px-2 mb-2 py-2" @click="selectImage" v-if="edit">
                         Select screenshot
                     </button><button class="mr-2 bg-slate-50 hover:bg-slate-100 shadow rounded px-2 mb-2 py-2" @click="post.screenshot = ''" v-if="edit">
                         Remove screenshot
@@ -69,7 +73,7 @@
                             ·
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer inline-flex -mt-1">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                            </svg> </span> <span class="text-xs ml-1" v-if="!edit && post?.user?._id === user?._id">
+                            </svg> </span> <span class="text-xs ml-1" v-if="!edit && (post?.user?._id === user?._id || user?.permissions?.includes('admin'))">
                             ·
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 cursor-pointer inline-flex -mt-0.5" @click="edit = true">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
@@ -155,6 +159,7 @@
             comment: "",
             edit: false,
             post: null,
+            statuses: [],
             moment
         }),
         created() {
@@ -192,7 +197,8 @@
                 await this.io.service("types/feedback").patch(this.post._id, {
                     title: this.post.title,
                     description: this.post.description,
-                    screenshot: this.post.screenshot
+                    screenshot: this.post.screenshot,
+                    status_id: this.post.status_id
                 });
                 await this.refresh();
                 this.edit = false;
@@ -207,7 +213,12 @@
             },
             async refresh() {
                 this.post = await this.io.service("types/feedback").get(this.id);
+                await this.getStatuses();
                 this.$emit("post", this.post)
+            },
+            async getStatuses() {
+                const statuses = await this.io.service("types/feedback-status").find();
+                this.statuses = statuses?.data || []
             },
             async myVote(post) {
                 const votes = await this.io.service("types/feedback-votes").find({
