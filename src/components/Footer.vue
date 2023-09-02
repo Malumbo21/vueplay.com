54b6c1f82764b5ca7800dbe37e23dfca1978a820
Vue Play <author@vueplay.com>
2023-09-02 17:50:13 +0200

    Stored by Vue Play

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,36 +1,42 @@
 <template>
-    <footer style="width:100%;text-align:center;box-sizing:border-box;display:inline-block" class="text-sky-100 bg-sky-950 py-12 text-xs md:text-md sm:text-sm px-6">
+	<footer
+		style="width:100%;text-align:center;box-sizing:border-box;display:inline-block"
+		class="py-20 text-sky-100 bg-sky-950 text-xs md:text-md sm:text-sm px-6">
 
-        <p class="pb-3">
-            © All rights reserved - Siglar Development AS {{
+		<p class="pb-3">
+			© All rights reserved -
+			Siglar Development AS {{
 new Date().getUTCFullYear()
 }}
-        </p> <router-link to="/release-log">
-            Release log
-        </router-link>
-        -
-        <router-link to="/terms-of-service">
-            Terms of Service
-        </router-link>-<router-link to="/privacy-policy">
-            Privacy Policy
-        </router-link>
-    </footer>
+		</p> <router-link
+			to="/release-log">
+			Release log
+		</router-link>
+		-
+		<router-link
+			to="/terms-of-service">
+			Terms of Service
+		</router-link>-<router-link
+			to="/privacy-policy">
+			Privacy Policy
+		</router-link>
+	</footer>
 </template>
 <script>
-    export default {
-        data: () => ({})
-    };
+	export default { data: () => ({}) };
+
 </script>
 <style scoped>
-    footer {
-        font-family: verdana;
-    }
+	footer {
+		font-family: verdana;
+	}
 
-    a {
-        text-decoration: none;
-    }
+	a {
+		text-decoration: none;
+	}
 
-    a:hover {
-        color: #ffffffff;
-    }
+	a:hover {
+		color: #ffffffff;
+	}
+
 </style>
