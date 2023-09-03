e8b02422a2376afabe96a9144a2759545a96f5d1
Vue Play <author@vueplay.com>
2023-09-03 22:01:11 +0200

    Stored by Vue Play

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -8,25 +8,28 @@
 			:class="{active: route === '/'}"
 			class="hover:text-green-700 text-slate-700 pr-2 sm:hidden">
 			Home
-		</button><button
-			@click="to('/features')"
+		</button><button @click="to('/features')"
 			:class="{active: route === '/features'}"
 			class="hover:text-green-700 text-slate-700 px-2">
 			Features
-		</button><button
-			@click="to('/pricing')"
+		</button><button @click="to('/pricing')"
 			:class="{active: route === '/pricing'}"
 			class="hover:text-green-700 text-slate-700 px-2">
 			Pricing
-		</button> <button
-			@click="submenu=true"
+		</button> <button @click="submenu=true"
 			:class="{active: subRoute}"
 			class="relative hover:text-green-700 text-slate-700 pr-2"
 			@mouseenter="submenu=true"
 			@mouseleave="submenu=false">
 			<span class="">
-				More...
-			</span>
+				More
+			</span><svg xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24" fill="currentColor"
+				class="w-4 h-4 inline-block">
+				<path fill-rule="evenodd"
+					d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"
+					clip-rule="evenodd" />
+			</svg>
 			<div class="bg-slate-50 border-y-slate-100 border-t border-b-2 right-0 top-full absolute shadow pb-2"
 				v-if="submenu">
 				<button
@@ -46,8 +49,7 @@
 					Documentation
 				</button>
 			</div>
-		</button> <a
-			:class="{active: route === '/app'}"
+		</button> <a :class="{active: route === '/app'}"
 			style="font-weight:bold;border-top-left-radius:4px;border-bottom-left-radius:4px;height:29px;border-top-right-radius:4px;border-bottom-right-radius:4px;margin-right:10px;padding-top:7px;padding-bottom:7px"
 			class="px-3 hover:bg-violet-600 bg-violet-500 from-blue-500 bg-gradient-to-br hover:from-blue-600 text-white duration-300 transition hover:drop-shadow-lg drop-shadow"
 			href="https://studio.vueplay.com">
@@ -56,24 +58,17 @@
 	</nav>
 </template>
 <script>
-	export default { data: () =>
-	({ submenu: false, next: "" }),
-		computed: { route() { return this
-					.next || this.$route
-					.path },
-		subRoute() { const routes = [
-					"/documentation",
+	export default { data: () => ({ submenu: false,
+			next: "" }), computed: { route() { return this
+					.next || this.$route.path },
+		subRoute() { const routes = ["/documentation",
 					"/feedback"
-				]; for (const route of
-						routes) { if (this
-						.$route.path
-						.includes(route))
-						return true } return false } },
-		methods: { open(url) { location
-					.href = url }, to(
+				]; for (const route of routes) { if (this
+						.$route.path.includes(route))
+					return true } return false } },
+		methods: { open(url) { location.href = url }, to(
 			to) { this.next = to;
-				this.$router.push(
-				to) } } };
+				this.$router.push(to) } } };
 
 </script>
 <style scoped>
