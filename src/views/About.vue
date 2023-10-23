e3e31c68c437cb03a1e73058a133f6c69ad18482
Vue Play <author@vueplay.com>
2023-10-23 21:46:25 +0200

    Stored by Vue Play

--- src/views/About.vue
+++ src/views/About.vue
@@ -78,9 +78,31 @@
 					<div class="bg-gradient-to-b from-slate-100 mb-10 pt-10 bg-white pb-10 flex">
 						<div class="place-content-between inline-flex p-5 max-w-screen-sm grow mx-auto">
 							<div class="bg-slate-200 p-2"></div>
+							<div class="bg-slate-200 p-2"><svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z"
+										clip-rule="evenodd"
+									/>
+								</svg></div>
 							<div class="bg-slate-200 p-2"></div>
-							<div class="bg-slate-200 p-2"></div>
-							<div class="bg-slate-200 p-2"></div>
+							<div class="bg-slate-200 p-2"><svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									class="w-6 h-6"
+								>
+									<path
+										fill-rule="evenodd"
+										d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z"
+										clip-rule="evenodd"
+									/>
+								</svg></div>
 							<div class="bg-slate-200 p-2"></div>
 						</div>
 					</div><a
