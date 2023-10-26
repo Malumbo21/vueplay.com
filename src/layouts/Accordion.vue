495c63d77ba0ae558a546da00ca7bbbf2d8e39b4
Vue Play <author@vueplay.com>
2023-10-26 21:36:49 +0200

    Stored by Vue Play

--- src/layouts/Accordion.vue
+++ src/layouts/Accordion.vue
@@ -1,70 +1,110 @@
 <template>
-	<div>
-		<details
-			class="group"
-			:open="isOpen"
+	<details
+		class="group"
+		:class="'vp-accordion-' + this.group"
+		:open="isOpen"
+	>
+		<summary
+			@click.prevent="isOpen = !isOpen"
+			class="hover:bg-slate-50 text-slate-800 border-b duration-75 transition-colors cursor-pointer bg-slate-100 p-4 place-content-between grow flex list-none"
 		>
-			<summary
-				@click.prevent="isOpen = !isOpen"
-				class="hover:bg-slate-50 text-slate-800 border-b duration-75 transition-colors cursor-pointer bg-slate-100 p-4 place-content-between grow flex list-none"
+			<span style="height:18px">
+				{{ title }} {{ id }}
+			</span> <svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="w-5 h-5 group-open:hidden"
 			>
-				<span style="height:18px">
-					Title
-				</span> <svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					class="w-5 h-5 group-open:hidden"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
-						clip-rule="evenodd"
-					/>
-				</svg> <svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="currentColor"
-					class="w-5 h-5 hidden group-open:inline"
-				>
-					<path
-						fill-rule="evenodd"
-						d="M3.75 12a.75.75 0 01.75-.75h15a.75.75 0 010 1.5h-15a.75.75 0 01-.75-.75z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</summary>
-			<div class="p-4">
-				<slot>
-					Lorem ipsum dolor sit amet
-				</slot>
-			</div>
-		</details>
-	</div>
+				<path
+					fill-rule="evenodd"
+					d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z"
+					clip-rule="evenodd"
+				/>
+			</svg> <svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="w-5 h-5 hidden group-open:inline"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M3.75 12a.75.75 0 01.75-.75h15a.75.75 0 010 1.5h-15a.75.75 0 01-.75-.75z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</summary>
+		<div class="p-4">
+			<slot>
+				Lorem ipsum dolor sit amet
+			</slot>
+		</div>
+	</details>
 </template>
 <script>
 	export default {
 		props: {
+			title: {
+				type: String,
+				default: 'Title'
+			},
 			open: {
-				default: false,
-				type: Boolean
+				type: Boolean,
+				default: false
+			},
+			group: {
+				type: String,
+				default: 'accordion'
 			}
 		},
+		emits: ['open', 'close'],
 		data: () => ({
-			isOpen: false
+			isOpen: false,
+			id: 0
 		}),
-		created() {
-			this.isOpen = this.open;
+		mounted() {
+			if (!document.accordion) document.accordion = {}
+			let i = 1
+			for (const element of document.querySelectorAll('.vp-accordion-' + this.group)) {
+				if (this.$el === element) this.id = i
+				i++
+			}
+			if (document.accordion[this.group]) {
+				this.isOpen = document.accordion[this.group] === this.id
+			} else {
+				this.isOpen = this.open;
+			}
+			document.body.addEventListener(this.group, this.listen)
+		},
+		beforeUnmount() {
+			document.body.removeEventListener(this.group, this.listen)
 		},
 		watch: {
-			open(open) {
-				this.isOpen = open;
-			},
 			isOpen(open) {
-				if (open !== this.open) this.$emit(open ? 'open' : 'close');
+				if (open) {
+					document.accordion[this.group] = this.id
+					this.$emit('open');
+				} else {
+					this.$emit('close');
+				}
+				this.emit({
+					id: this.id,
+					open
+				})
+			}
+		},
+		methods: {
+			emit(event) {
+				document.body.dispatchEvent(new CustomEvent(this.group, {
+					detail: event
+				}))
+			},
+			listen(event) {
+				if (event.detail.open && event.detail.id !== this.id) {
+					this.isOpen = false
+				}
 			}
 		}
 	};
 
 </script>
-<style scoped></style>
---
+++ src/layouts/TestAccordion.vue
@@ -0,0 +1,43 @@
+<template>
+	<Accordion>
+		<b title="This is the title">
+			bold text is awesome
+		</b>
+	</Accordion>
+	<Accordion>
+		<b title="This is the title">
+			bold text is awesome 2
+		</b>
+	</Accordion>
+	<Accordion>
+		<b title="This is the title">
+			bold text is awesome 3
+		</b>
+	</Accordion>
+	<Accordion group="group2">
+		<b title="This is the title">
+			bold text is awesome 3
+		</b>
+	</Accordion>
+	<Accordion group="group2">
+		<b title="This is the title">
+			bold text is awesome 3
+		</b>
+	</Accordion>
+	<Accordion group="group2">
+		<b title="This is the title">
+			bold text is awesome 3
+		</b>
+	</Accordion>
+</template>
+<script>
+	import Accordion from '@/layouts/Accordion.vue';
+	export default {
+		components: {
+			Accordion: Accordion
+		},
+		data: () => ({})
+	};
+
+</script>
+<style scoped></style>
