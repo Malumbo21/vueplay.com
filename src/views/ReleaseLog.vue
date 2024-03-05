ac9a6b9379c3a69c44d74313ce24666a0fe3b1ec
Vue Play <author@vueplay.com>
2024-03-05 12:51:36 +0100

    Stored by Vue Play

--- src/views/ReleaseLog.vue
+++ src/views/ReleaseLog.vue
@@ -14,6 +14,16 @@
 		<main class="p-6 m-auto max-w-xl container">
 			<section class="shadow border mb-6 rounded-lg bg-white p-6">
 				<h3>
+					05.03.2024 - Release 1.2.2
+				</h3>
+				<ul class="indent-0 ml-5 list-disc">
+					<li>
+						Enhancement - Updated starter kit
+					</li>
+				</ul>
+			</section>
+			<section class="shadow border mb-6 rounded-lg bg-white p-6">
+				<h3>
 					04.03.2024 - Release 1.2.1
 				</h3>
 				<ul class="indent-0 ml-5 list-disc">
