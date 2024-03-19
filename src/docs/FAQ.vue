dcdcf83c465c95e7755fe753a5152580596a66e9
Vue Play <author@vueplay.com>
2024-03-19 11:59:35 +0100

    Stored by Vue Play

--- src/docs/FAQ.vue
+++ src/docs/FAQ.vue
@@ -74,14 +74,14 @@
 					Vue Play is built up using Vue.js SFC's (Vue Single File Components) which are files ending with the .vue extension. This files houses specific functionalities + templates and works like modules in a webpage. This modules will be available as Components in Vue Play, to be dragged into any playground. It can be authored by developers from us or the community.
 				</p>
 			</Accordion>
-			<Accordion title="How can I get premade components into my application?">
+			<Accordion title="How can I insert HTML snippets?">
 				<span class="text-lg">
-					You can paste pre-made tailwind components into the playground using the Context Menu (Three Dots) or Keyboard Shortcuts
+					You can paste any HTML block into the playground using the Context Menu (Three Dots) or Keyboard Shortcut
 				</span><span
 					class="px-1.5 rounded-md p-1 text-emerald-800 bg-emerald-300 text-lg"
 					style="white-space:nowrap"
 				>
-					{{ isMac ? 'cmd + v' : 'ctrl + v' }}
+					{{ isMac ? 'CMD + V' : 'CTRL + V' }}
 				</span><span class="text-lg">
 					while you are in the playground.
 				</span>
