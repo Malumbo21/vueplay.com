11f0e9f30e375aaa9592889b78048c1d645a2dcf
Vue Play <author@vueplay.com>
2024-11-08 16:59:17 +0100

    Stored by Vue Play

--- src/views/About.vue
+++ src/views/About.vue
@@ -216,7 +216,7 @@
 						style="margin-bottom:100px;color:#15244fff;margin-top:75px;width:calc(100% - 40px);margin-left:20px"
 						class="text-4xl md:text-slate-900 font-bold"
 					>
-						2400+ Registered Users 🚀
+						2450+ Registered Users 🚀
 					</h2>
 					<h2
 						style="margin-bottom:50px;max-width:450px;color:#15244fff"
