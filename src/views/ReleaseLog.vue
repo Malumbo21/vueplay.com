15834f4865e7580e813fcb1e9593f5b5752f5047
Vue Play <author@vueplay.com>
2024-11-27 16:42:25 +0100

    Stored by Vue Play

--- src/views/ReleaseLog.vue
+++ src/views/ReleaseLog.vue
@@ -14,6 +14,22 @@
 		<main class="p-6 m-auto max-w-xl container">
 			<section class="shadow border mb-6 rounded-lg bg-white p-6">
 				<h3>
+					27.11.2024 - Release 1.4.0
+				</h3>
+				<ul class="indent-0 ml-5 list-disc">
+					<li>
+						Feature - All style controllers available for free
+					</li>
+					<li>
+						Feature - All elements available for free
+					</li>
+					<li>
+						Feature - Improved backend API
+					</li>
+				</ul>
+			</section>
+			<section class="shadow border mb-6 rounded-lg bg-white p-6">
+				<h3>
 					15.07.2024 - Release 1.3.2
 				</h3>
 				<ul class="indent-0 ml-5 list-disc">
