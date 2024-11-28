74d2b32ee3b1f6f05c090505cf804544c5d13a87
Vue Play <author@vueplay.com>
2024-11-28 12:22:55 +0100

    Stored by Vue Play

--- src/views/ReleaseLog.vue
+++ src/views/ReleaseLog.vue
@@ -14,6 +14,16 @@
 		<main class="p-6 m-auto max-w-xl container">
 			<section class="shadow border mb-6 rounded-lg bg-white p-6">
 				<h3>
+					28.11.2024 - Release 1.4.2
+				</h3>
+				<ul class="indent-0 ml-5 list-disc">
+					<li>
+						Feature - Fix dependency injection
+					</li>
+				</ul>
+			</section>
+			<section class="shadow border mb-6 rounded-lg bg-white p-6">
+				<h3>
 					27.11.2024 - Release 1.4.1
 				</h3>
 				<ul class="indent-0 ml-5 list-disc">
