c2fbc569ce099639c41484eaa7263d125771a6f1
Vue Play <author@vueplay.com>
2025-03-13 22:01:55 +0100

    Stored by Vue Play

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,29 +1,32 @@
 <template>
 	<footer
 		style="width:100%;text-align:center;box-sizing:border-box;display:inline-block"
-		class="py-20 text-sky-100 bg-sky-950 text-xs md:text-md sm:text-sm px-6">
+		class="py-20 text-sky-100 bg-sky-950 text-xs md:text-md sm:text-sm px-6"
+	>
 
-		<p class="pb-3">
+		<p
+			class="pb-3"
+			data-allow-mismatch=""
+		>
 			© All rights reserved -
 			Siglar Development AS {{
 new Date().getUTCFullYear()
 }}
-		</p> <router-link
-			to="/release-log">
+		</p> <router-link to="/release-log">
 			Release log
 		</router-link>
 		-
-		<router-link
-			to="/terms-of-service">
+		<router-link to="/terms-of-service">
 			Terms of Service
-		</router-link>-<router-link
-			to="/privacy-policy">
+		</router-link>-<router-link to="/privacy-policy">
 			Privacy Policy
 		</router-link>
 	</footer>
 </template>
 <script>
-	export default { data: () => ({}) };
+	export default {
+		data: () => ({})
+	};
 
 </script>
 <style scoped>
