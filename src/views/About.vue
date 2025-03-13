5a8e60bf7e89797a5ec3ed31062ec0245d28ff9e
Vue Play <author@vueplay.com>
2025-03-13 21:44:29 +0100

    Stored by Vue Play

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
 	<div class="bg-slate-100 flex flex-col h-screen overflow-auto">
 		<Meta />
 		<VueNavigator class="mb-5 z-20" />
-		<header class="">
+		<header>
 			<div
 				style="background-size:contain;background-position:center;background-repeat:no-repeat;width:266px;height:125px;margin-right:auto;margin-left:auto;margin-top:0px;position:relative;max-width:calc(100% - 100px);margin-bottom:-20px;left:5px"
 				class="logo"
